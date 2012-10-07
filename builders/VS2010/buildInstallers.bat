afe8f3721c73d88550cef73685c985c73a196ebf
michael cullen <michael@cullen-online.com>
2012-10-07 22:37:53 +0100

    fixed build installer script

--- builders/VS2010/buildInstallers.bat
+++ builders/VS2010/buildInstallers.bat
@@ -3,9 +3,15 @@ set version=0.0.0.0
 ) ELSE (
 set version=%1
 )
+
+IF EXIST "C:\Program Files (x86)\Windows Installer XML v3.6\bin" (
+set WIXPATH="C:\Program Files (x86)\Windows Installer XML v3.6\bin"
+) ELSE (
+set WIXPATH="C:\Program Files (x86)\WiX Toolset v3.6\bin"
+)
  
- "C:\Program Files (x86)\Windows Installer XML v3.6\bin\candle.exe" crtmpserver.wxs -out "crtmpserver86.wixobj" -dPlatform=x86 -dVersion="%version%" -ext WixUIExtension 
- "C:\Program Files (x86)\Windows Installer XML v3.6\bin\light.exe" -nologo crtmpserver86.wixobj -out "crtmpserver x86 %version%.msi"  -ext WixUIExtension
+ %WIXPATH%\candle.exe crtmpserver.wxs -out "crtmpserver86.wixobj" -dPlatform=x86 -dVersion="%version%" -ext WixUIExtension 
+ %WIXPATH%\light.exe -nologo crtmpserver86.wixobj -out "crtmpserver x86 %version%.msi"  -ext WixUIExtension
  
- "C:\Program Files (x86)\Windows Installer XML v3.6\bin\candle.exe" crtmpserver.wxs -out "crtmpserver64.wixobj" -dPlatform=x64  -dVersion="%version%" -ext WixUIExtension 
- "C:\Program Files (x86)\Windows Installer XML v3.6\bin\light.exe" -nologo crtmpserver64.wixobj -out "crtmpserver x64 %version%.msi"  -ext WixUIExtension
+ %WIXPATH%\candle.exe crtmpserver.wxs -out "crtmpserver64.wixobj" -dPlatform=x64  -dVersion="%version%" -ext WixUIExtension 
+ %WIXPATH%\light.exe -nologo crtmpserver64.wixobj -out "crtmpserver x64 %version%.msi"  -ext WixUIExtension
